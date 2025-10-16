8fc6ac052a177fa22d221ce8f7224a855d84991d
sajeev402 <sajeevthota143@gmail.com>
2025-10-16 11:01:47 +0530

    16th act Taskfrom completely

--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,19 +1,37 @@
-export default function Taskform() {
+import { useState } from "react"
+
+export default function Taskform({addTask}) {
+    const[task,setTask] = useState('');
+    const[priority, setPriority] = useState('medium');
+    const[category, setCategory] = useState('general');
+
+    const handlelesubmit = (e) => {
+        e.preventDefault();
+        addTask({text: task, priority, category, completed: false});
+
+        //reset
+        setTask('');
+        setPriority('medium');
+        setCategory('general');
+    }
+
     return(
-        <form>
+        <form onSubmit={handlelesubmit}>
             <div>
-                <input type="text" placeholder="Enter the task"/>
+                <input type="text" placeholder="Enter the task"
+                onChange={(e) => setTask(e.target.value)} value={task}/>
                 <button type="submit">Add Task</button>
+                <h1>{task} {priority} {category} </h1>
             </div>
 
             <div>
-                <select>
+                <select value={priority} onChange={(e) => setPriority(e.target.value)}>
                     <option value="high">High</option>
                     <option value="medium">Medium</option>
                     <option value="low">low</option>
                 </select>
 
-                <select>
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
                     <option value="general">General</option>
                     <option value="work">work</option>
                     <option value="personal">Personal</option>
